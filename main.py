f1a9236cdfeb42b8df814dc04ad50bb300fe136c
cosmcbun <cosmcbun+github@gmail.com>
2023-11-12 17:23:08 -0600

    Added validation/testing split

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 import datetime, re
 import nltk
 import warnings
 import pickle
 import os
+import random
 
 warnings.filterwarnings(action='ignore')
 
@@ -11,8 +13,8 @@ import gensim
 from gensim.models import Word2Vec
 
 def createPost(row):
-    global posts
-    posts.append(Post(row))
+    global all_posts
+    all_posts.append(Post(row))
 
 class Post:
     def __init__(self, row):
@@ -67,16 +69,15 @@ class Post:
 
     def tokenize(self):
         #wnl = nltk.stem.WordNetLemmatizer()
-        #tokens = nltk.tokenize.word_tokenize(self.body)
+        self.tokens = nltk.tokenize.word_tokenize(self.body)
         #for t in tokens:
         #    print(wnl.lemmatize(t, pos="v"))
-        pass
 
     def __repr__(self):
         return self.title
 
 def concatAllPosts(posts):
-    return "".join([p.body for p in posts])
+    return " ".join([p.body for p in posts])
 
 def trainWord2Vec(posts):
     tokenized_data = []
@@ -89,25 +90,40 @@ def trainWord2Vec(posts):
     model = gensim.models.Word2Vec(tokenized_data, min_count=1, vector_size=100, window=5, sg=1)
     return model
 
+def getAverageTokenVector(post, model):
+    tokens = nltk.tokenize.word_tokenize(post.body)
+    # tokens = ["my", "girlfriend", "hates", "me"]#nltk.tokenize.word_tokenize(post.body)
+    vectors = np.asarray([model.wv[word] for word in tokens])
+    return np.mean(vectors, axis=0)
+
+    # avg vectors
+    # return avg vector
+
 start_time = datetime.datetime.now()
 if os.path.isfile("posts.pickle"):
-    posts = pickle.load(open("posts.pickle", "rb"))
+    all_posts = pickle.load(open("posts.pickle", "rb"))
     print("Post loading complete")
 else:
     data = pd.read_csv("./aita_clean.csv")
-    posts = []
+    all_posts = []
     data.apply(createPost, axis=1)
+    random.seed(42)
+    random.shuffle(all_posts)
     print("Post compilation complete")
-    pickle.dump(posts, open("posts.pickle", "wb"))
+    pickle.dump(all_posts, open("posts.pickle", "wb"))
+training_set = all_posts[:80000]
+validation_set = all_posts[80000:90000]
+testing_set = all_posts[90000:]
+print([len(s) for s in [training_set,validation_set,testing_set]])
 
 print(datetime.datetime.now()-start_time)
-print(len(posts))
+print(len(all_posts))
 
 if os.path.isfile("model.pickle"):
     model = pickle.load(open("model.pickle", "rb"))
     print("Model loading complete")
 else:
-    model = trainWord2Vec(posts)
+    model = trainWord2Vec(training_set)
     print("Model compilation complete")
     pickle.dump(model, open("model.pickle", "wb"))
 
@@ -115,8 +131,18 @@ print(datetime.datetime.now()-start_time)
 
 print("Cosine similarity between 'h' " +
       "and 'w' - Skip Gram : ",
-      model.wv.similarity('lose', 'lifestyle'))
+      model.wv.similarity('happy', 'glad'))
+print("Cosine similarity between 'h' " +
+      "and 'w' - Skip Gram : ",
+      model.wv.similarity('girlfriend', 'wife'))
 print("Cosine similarity between 'h' " +
       "and 'p' - Skip Gram : ",
       model.wv.similarity('the', 'a'))
-print(model.wv)
+
+all_posts[41].body= "wife"
+a = getAverageTokenVector(all_posts[41], model)
+all_posts[41].body= "a"
+b = getAverageTokenVector(all_posts[41], model)
+print(sum([abs(n) for n in a]))
+print(sum([abs(n) for n in b]))
+print(a-b)
