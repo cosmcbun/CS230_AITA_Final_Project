0b60010ef3637b2bd24825b5ed66fb34ef300347
cosmcbun <cosmcbun+github@gmail.com>
2023-11-17 17:34:08 -0600

    Revised word tokenization

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-from tensorflow import keras
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.losses import BinaryCrossentropy
+#from tensorflow import keras
+#from tensorflow.keras.layers import Dense
+#from tensorflow.keras.losses import BinaryCrossentropy
 import pandas as pd
 import numpy as np
 import datetime, re
 import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 import warnings
 import pickle
 import os
 import random
-
-VECTOR_SIZE = 100
+import string
 
 warnings.filterwarnings(action='ignore')
 
 import gensim
 from gensim.models import Word2Vec
 
-def createPost(row):
-    global all_posts
+def addPostObjectToList(row, posts):
     post = Post(row)
-    if post.body: all_posts.append(post)
+    if post.body: posts.append(post)
+    if len(posts)%10000 == 0: print(f"Post loading {len(posts)/1000}% complete")
 
 class Post:
     def __init__(self, row):
@@ -32,6 +33,8 @@ class Post:
         self.extractDemographics()
         self.stripFunctionalWords()
         self.cleanBody()
+
+        self.tokens = []
         self.tokenize()
 
     def convertTimestamp(self):
@@ -74,10 +77,15 @@ class Post:
         self.body = self.body.lower().replace("\n"," ")
 
     def tokenize(self):
-        #wnl = nltk.stem.WordNetLemmatizer()
-        self.tokens = nltk.tokenize.word_tokenize(self.body)
-        #for t in tokens:
-        #    print(wnl.lemmatize(t, pos="v"))
+        stop_words = set(stopwords.words('english'))
+        printable = set(string.ascii_letters)
+        printable.add("'")
+        printable.add(" ")
+        lemmatizer = WordNetLemmatizer()
+
+        for word in ''.join(filter(lambda x: x in printable, self.body)).split(" "):
+            if word and word not in stop_words and "www" not in word:
+                self.tokens.append(lemmatizer.lemmatize(word))
 
     def __repr__(self):
         return self.title
@@ -93,49 +101,15 @@ def trainWord2Vec(posts):
             temp.append(j)
         tokenized_data.append(temp)
 
-    model = gensim.models.Word2Vec(tokenized_data, min_count=1, vector_size=VECTOR_SIZE, window=5, sg=1)
+    model = gensim.models.Word2Vec(tokenized_data, min_count=1, vector_size=100, window=5, sg=1)
     return model
 
 def getAverageTokenVector(post, model):
     tokens = post.tokens
-    # tokens = ["my", "girlfriend", "hates", "me"]#nltk.tokenize.word_tokenize(post.body)
     vectors = np.asarray([(model.wv[word] if word in model.wv else np.zeros(VECTOR_SIZE)) for word in tokens])
     return np.mean(vectors, axis=0)
 
-start_time = datetime.datetime.now()
-if os.path.isfile("posts.pickle"):
-    all_posts = pickle.load(open("posts.pickle", "rb"))
-    print("Post loading complete")
-else:
-    data = pd.read_csv("./aita_clean.csv")
-    all_posts = []
-    data.apply(createPost, axis=1)
-    random.seed(42)
-    random.shuffle(all_posts)
-    print("Post compilation complete")
-    pickle.dump(all_posts, open("posts.pickle", "wb"))
-    print(datetime.datetime.now()-start_time)
-
-for i in range(len(all_posts) - 1, -1, -1):
-    if len(all_posts[i].tokens) < 10:
-        all_posts.pop(i)
-
-training_set = all_posts[:80000]
-validation_set = all_posts[80000:90000]
-testing_set = all_posts[90000:]
-
-
-if os.path.isfile("model.pickle"):
-    model = pickle.load(open("model.pickle", "rb"))
-    print("Model loading complete")
-else:
-    model = trainWord2Vec(training_set)
-    print("Model compilation complete")
-    pickle.dump(model, open("model.pickle", "wb"))
-    print(datetime.datetime.now()-start_time)
-
-
-def create_neural_network():
+def createNeuralNetwork():
   model = keras.Sequential()
   model.add(keras.Input(shape=(VECTOR_SIZE)))
 
@@ -151,7 +125,7 @@ def create_neural_network():
 
   return model
 
-def posts_to_dataset(posts, model):
+def postsToDataset(posts, model):
     inputs = np.ndarray((len(posts), VECTOR_SIZE))
     for i, post in enumerate(posts):
         inputs[i] = getAverageTokenVector(post, model)
@@ -159,7 +133,44 @@ def posts_to_dataset(posts, model):
     outputs = np.array([post.is_asshole for post in posts])
     return inputs, outputs
 
-neural_net = create_neural_network()
+def getAllPosts():
+    if os.path.isfile("posts.pickle"):
+        return pickle.load(open("posts.pickle", "rb"))
+    else:
+        data = pd.read_csv("./aita_clean.csv")
+        all_posts = []
+        data.apply(lambda row: addPostObjectToList(row, all_posts), axis=1)
+        random.seed(42)
+        random.shuffle(all_posts)
+        pickle.dump(all_posts, open("posts.pickle", "wb"))
+        return all_posts
+
+def getWord2Vec(word_set):
+    if os.path.isfile("model.pickle"):
+        return pickle.load(open("model.pickle", "rb"))
+    else:
+        model = trainWord2Vec(word_set)
+        pickle.dump(model, open("model.pickle", "wb"))
+        return model
+
+#dylan you have to run this once
+#nltk.download('stopwords')
+
+VECTOR_SIZE = 100
+
+start_time = datetime.datetime.now()
+all_posts = getAllPosts()
+training_set = all_posts[:80000]
+validation_set = all_posts[80000:90000]
+testing_set = all_posts[90000:]
+print("Post compilation complete")
+print(datetime.datetime.now()-start_time)
+
+word2Vec = getWord2Vec(training_set)
+print("Model compilation complete")
+print(datetime.datetime.now() - start_time)
+
+"""neural_net = create_neural_network()
 print("created neural network!")
 train_ds = posts_to_dataset(training_set, model)
 validation_ds = posts_to_dataset(validation_set, model)
@@ -171,4 +182,4 @@ history_basic_model = neural_net.fit(train_ds[0], train_ds[1], epochs=25, valida
 print(history_basic_model.history)
 # results = neural_net.evaluate(test_ds[0], test_ds[1])
 # print(len(results))
-# print("Test Loss:", results[0], "Test Accuracy:", results[1])
+# print("Test Loss:", results[0], "Test Accuracy:", results[1])"""
